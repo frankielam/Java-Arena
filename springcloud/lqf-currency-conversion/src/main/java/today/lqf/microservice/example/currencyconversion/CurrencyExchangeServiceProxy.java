96e2a948ad4c7e0b8f094065fe5da9e5c9a46572
frankielam <frankie.lqf@gmail.com>
2021-11-23 10:32:40 +0800

    Spring Cloud demo-1 finally version
    SpringBoot 2.5.7
    SpringCloud 2020.0.4
    Eureka 3.0.3
    OpenFeign 3.0.4

--- springcloud/lqf-currency-conversion/src/main/java/today/lqf/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
+++ springcloud/lqf-currency-conversion/src/main/java/today/lqf/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,12 @@
 package today.lqf.microservice.example.currencyconversion;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "forex-service", url = "127.0.0.1:8000")
+@FeignClient(name = "forex-service")
 @Component
 public interface CurrencyExchangeServiceProxy {
 
--- springcloud/lqf-forex-service/src/main/java/today/lqf/microservice/example/forex/LqfForexServiceApplication.java
+++ springcloud/lqf-forex-service/src/main/java/today/lqf/microservice/example/forex/LqfForexServiceApplication.java
@@ -2,8 +2,10 @@ package today.lqf.microservice.example.forex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LqfForexServiceApplication {
 
     public static void main(String[] args) {
