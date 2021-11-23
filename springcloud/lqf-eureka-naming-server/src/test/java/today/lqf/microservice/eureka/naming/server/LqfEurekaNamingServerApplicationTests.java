3c1d5b30b724c1d20eef2aae429addb90513d257
frankielam <frankie.lqf@gmail.com>
2021-11-23 10:30:53 +0800

    Eureka server

---
+++ springcloud/lqf-eureka-naming-server/src/test/java/today/lqf/microservice/eureka/naming/server/LqfEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package today.lqf.microservice.eureka.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LqfEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
