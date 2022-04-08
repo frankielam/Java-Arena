eb49c23eac021f7ef8766dff793eacad1c4c1bc1
frankielam <frankie.lqf@gmail.com>
2022-04-08 23:58:56 +0800

    LeetCode coding

---
+++ leetcode/src/today/lqf/leetcode/LeetCode429.java
@@ -0,0 +1,68 @@
+package today.lqf.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LeetCode429 {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        List<Integer> subList = new ArrayList<>();
+        queue.add(root);
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node == null) {
+                if (subList.size() > 0) {
+                    list.add(subList);
+                }
+                subList = new ArrayList<>();
+            } else {
+                subList.add(node.val);
+
+                List<Node> childs = node.children;
+                if (childs.size() > 0) {
+                    for (Node child : childs) {
+                        queue.add(child);
+                    }
+                }
+                if (queue.size() > 0 && queue.element() == null) {
+                    queue.add(null);
+                }
+            }
+        }
+        if (subList.size() > 0) {
+            list.add(subList);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
