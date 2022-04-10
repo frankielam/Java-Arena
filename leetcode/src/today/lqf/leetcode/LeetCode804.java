11a80d5a295c94177ed7cd7e6d0def852b00dc4c
frankielam <frankie.lqf@gmail.com>
2022-04-11 00:03:07 +0800

    LeetCode coding

--- leetcode/src/today/lqf/leetcode/LeetCode804.java
+++ leetcode/src/today/lqf/leetcode/LeetCode804.java
@@ -10,10 +10,19 @@ public class LeetCode804 {
                 "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
         Set<String> set = new HashSet<>();
         for (int i = 0; i < words.length; i++) {
-
+            StringBuffer sb = new StringBuffer();
+            for (int j = 0; j < words[i].length(); j++) {
+                int k = words[i].charAt(j) - 'a';
+                sb.append(code[k]);
+            }
+            set.add(sb.toString());
         }
 
         return set.size();
     }
 
+    public static void main(String[] arg) {
+        LeetCode804 obj = new LeetCode804();
+        System.out.println(obj.uniqueMorseRepresentations(new String[] { "gin", "zen", "gig", "msg" }));
+    }
 }
