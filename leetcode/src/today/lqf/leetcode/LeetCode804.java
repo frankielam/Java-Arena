748b06e1f5e28bd574be836d67acfd1b1c8b3498
frankielam <frankie.lqf@gmail.com>
2022-04-10 23:59:29 +0800

    LeetCode coding

---
+++ leetcode/src/today/lqf/leetcode/LeetCode804.java
@@ -0,0 +1,19 @@
+package today.lqf.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode804 {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] code = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+
+        }
+
+        return set.size();
+    }
+
+}
