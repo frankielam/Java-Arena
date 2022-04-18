9959e9ad616bc7f9386602dff02118c687b1721c
frankielam <frankie.lqf@gmail.com>
2022-04-18 23:44:59 +0800

    LeetCode coding

---
+++ leetcode/src/today/lqf/leetcode/LeetCode386.java
@@ -0,0 +1,33 @@
+package today.lqf.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode386 {
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>();
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            list.add(num);
+            if (num * 10 <= n) {
+                num *= 10;
+            } else {
+                while (num % 10 == 9 || num + 1 > n) {
+                    num /= 10;
+                }
+                num++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LeetCode386 obj = new LeetCode386();
+        System.out.println(obj.lexicalOrder(13));
+        System.out.println(obj.lexicalOrder(2));
+        System.out.println(obj.lexicalOrder(192));
+
+    }
+
+}
